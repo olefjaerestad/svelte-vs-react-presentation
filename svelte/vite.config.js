7821739d2627e62ee8af0d29be78d63e2bf0737a
Ole Kristian Fjærestad <ole.kristian.fjarestad@nrk.no>
2021-11-28 10:51:09 +0100

    Run Svelte on a different port than React.

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -3,10 +3,13 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  server: {
+    port: 3001,
+  },
   plugins: [
     svelte({
       preprocess: [
-        // Compile TS
+        // Add support for TS/SCSS/etc
         autoPreprocess()
       ]
     })
